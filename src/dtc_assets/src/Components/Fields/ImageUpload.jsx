e4ceda1618ece4c01532ccb6ab17ba49fe1c785d
Jesse Williams <jessewilliams@Jesses-MBP.lan>
2021-10-28 14:52:41 -0500

    initial commit

--- src/dtc_assets/src/Components/Fields/ImageUpload.jsx
+++ src/dtc_assets/src/Components/Fields/ImageUpload.jsx
@@ -5,43 +5,39 @@ const ImageUpload = (props) => {
         label,
         disabled
     } = props;
-    
-    const inputRef = useRef();
-    let image;
-    let imageSrc = '';
-    const reader = new FileReader();
+    let inputRef = useRef();
+    const [imageSrc, setImageSrc]  =useState('');
 
-    const onUpload = () => {
-        image =inputRef.current.files[0];
-        console.log(1);
-        if(image){
-            return new Promise((resolve,reject) => {
-                console.log(2);
-                reader.onerror = () => {
-                    reader.abort();
-                    reject(new DOMException("Problem parsing input file."));
-                };
-                //TODO: figure out why the eventListeners aren't firing for reader
-                reader.addEventListener("load", function(){
-                    console.log(3);
-                    resolve(reader.result);
-                    imageSrc = reader.result;
-                    reader.readAsDataURL(image);
-                }) ;
-            })
-            
-        } else {}
+    const displayUploadedImage = (inputImage) => {
+        const reader = new FileReader();
+
+        return new Promise((resolve, reject) => {
+            reader.onload = () => {
+                resolve(reader.result);
+            }
+            reader.readAsDataURL(inputImage)
+        
+        });
+    }; 
+
+    const handleUpload = async () => {
+        const image = inputRef.current.files[0];
+        try{
+            setImageSrc(await displayUploadedImage(image));
+        } catch(e) {
+            console.warn(e.message);
+        }
     };
-    
 
     return(
         <div className={'imageDiv__' + label}>
             <label className={'ImageLabelDiv'}>
                 {label}
             </label>
-            <input id={'uploadedImaged'} type="file" className={'image'} ref={inputRef} onChange={onUpload}/>
+            <input id={'uploadedImaged'} type="file" className={'image'} ref={inputRef} onChange={handleUpload}/>
             <div className={"imagePreview"} id={"imagePreview"}>
                 <img src={imageSrc} alt="image preview" className="imagePreview__image"/>
+                
                 <span className="imagePreview__default-display">
                     Image Preview
                 </span>
