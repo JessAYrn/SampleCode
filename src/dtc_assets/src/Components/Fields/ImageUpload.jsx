111418a33666d412297cf85111d5db069505c354
Jesse Williams <jessewilliams@Jesses-MBP.lan>
2021-11-01 17:34:58 -0500

    initial commit

--- src/dtc_assets/src/Components/Fields/ImageUpload.jsx
+++ src/dtc_assets/src/Components/Fields/ImageUpload.jsx
@@ -1,4 +1,6 @@
 import React, {useRef, useState} from 'react';
+import InputBox from './InputBox';
+import "./ImageUpload.scss";
 
 const ImageUpload = (props) => {
     const {
@@ -6,7 +8,7 @@ const ImageUpload = (props) => {
         disabled
     } = props;
     let inputRef = useRef();
-    const [imageSrc, setImageSrc]  =useState('');
+    const [imageSrc, setImageSrc]  = useState("../../../assets/AddImageIcon.png");
 
     const displayUploadedImage = (inputImage) => {
         const reader = new FileReader();
@@ -30,17 +32,24 @@ const ImageUpload = (props) => {
     };
 
     return(
-        <div className={'imageDiv__' + label}>
-            <label className={'ImageLabelDiv'}>
-                {label}
-            </label>
-            <input id={'uploadedImaged'} type="file" className={'image'} ref={inputRef} onChange={handleUpload}/>
+        <div className={'imageDiv'}>
             <div className={"imagePreview"} id={"imagePreview"}>
                 <img src={imageSrc} alt="image preview" className="imagePreview__image"/>
-                
-                <span className="imagePreview__default-display">
-                    Image Preview
-                </span>
+                <InputBox
+                    label={""} 
+                />
+                {/* <video width="320" height="240" controls>
+                    <source src={imageSrc} type="video/mp4"/>
+                    <source src={imageSrc} type="video/ogg"/>
+                        Your browser does not support the video tag.
+                </video>                */}
+                {
+                    !imageSrc && 
+                    <span className="imagePreview__default-display">
+                        Image Preview
+                    </span>   
+                }
+                <input id={'uploadedImaged'} type="file" className={'imageInputButton'} ref={inputRef} onChange={handleUpload}/>
             </div>
         </div>
     );
